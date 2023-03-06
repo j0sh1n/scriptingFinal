409f605f81ed31e2d192aabb9a7fda31c9abf112
Joshua Pascual <joshuapascual@Danicas-iMac.local>
2023-03-07 02:24:48 +0800

    85%

--- src/spiels/Education.js
+++ src/spiels/Education.js
@@ -15,16 +15,34 @@ const Education = () => {
 
             <p>What is the highest level of education have you completed?</p><br />
 
-            {value === 'In College' && <div><p><input type="text" /> Have you graduated? What degree/course/school?</p></div>}
-            {value === 'Graduated' && <div><p><input type="text" /> Do you have pending board exam/result/licensure exam?</p><div><button onClick={() => setExtra(true)}>Yes</button> <button onClick={() => setExtra(false)}>No</button></div></div>}
-            { extra && <div><p><input type="text" /> When and what are your plans?</p></div>}
+            {value === 'In College' &&
+                <div>
+                    <p>
+                        <input type="text" />
+                        Have you graduated? What degree/course/school?
+                    </p>
+                </div>}
+            {value === 'Graduated' &&
+                <div className='flex-c'>
+                    <p>
+                        <input type="text" />
+                        Do you have pending board exam/result/licensure exam?
+                    </p>
+                    <div>
+                        <button onClick={() => setExtra(true)}>Yes</button>
+                        <button onClick={() => setExtra(false)}>No</button>
+                    </div>
+                </div>}
+
+
+            {extra && <div><p><input type="text" /> When and what are your plans?</p></div>}
             {value === 'Undergraduate' && <div><p><input type="text" /> What is your degree/course and your year level completed? Which school you attended?</p></div>}
 
             <br />
             <div className='education'>
-                <button onClick={() => {setValue('In College'); setExtra(false); }}>In College</button>
-                <button onClick={() => {setValue('Graduated'); setExtra(false);}}>Graduated</button>
-                <button onClick={() => {setValue('Undergraduate'); setExtra(false);}}>Undergraduate</button>
+                <button onClick={() => { setValue('In College'); setExtra(false); }}>In College</button>
+                <button onClick={() => { setValue('Graduated'); setExtra(false); }}>Graduated</button>
+                <button onClick={() => { setValue('Undergraduate'); setExtra(false); }}>Undergraduate</button>
             </div>
             <br />
             <br />
